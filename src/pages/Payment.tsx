af0ef6cc1760e3096e460743d0003de7d600b850
rashmikaz <phrashmika@gmail.com>
2025-02-18 20:18:49 +0530

    other pages

---
+++ src/pages/Payment.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+    return (
+        <div className="p-6">
+            <h1>payment</h1>
+        </div>
+    )
+}
---
+++ src/pages/Teacher.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+    return (
+        <div className="p-6">
+            <h1>teacher</h1>
+        </div>
+    )
+}
