0e9b7ec11e1c2eeb8f5356ec33f74e3e462a1cb9
rashmikaz <phrashmika@gmail.com>
2025-02-18 21:02:07 +0530

    path done complete

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -6,16 +6,20 @@ export function RootLayout() {
     const location = useLocation()
 
     const routeTitles: any = {
+        "/": "Homes",
+        "/student": "student Management",
+        "/teacher": "teacher Management",
+        "/payment": "payment Management"
 
     }
 
-    const title = routeTitles[location?.pathname] || "Shop"
+    const title = routeTitles[location?.pathname] || "shop"
 
     return (
         <div className="flex h-screen">
             <Menu />
             <div className="flex-1 flex flex-col transition-all duration-300">
-                <header className="bg-gray-800 text-white p-4 flex items-center">
+                <header className="text-black p-4 flex items-center">
                     <h1 className="text-xl font-semibold">{title}</h1>
                 </header>
                 <main className="p-4 flex-1 overflow-y-auto">
