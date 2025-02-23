fc4fb9b706e5b810947a73b8c89237fdd3f391bb
rashmikaz <phrashmika@gmail.com>
2025-02-23 09:34:40 +0530

    teacher form

--- src/pages/Teacher.tsx
+++ src/pages/Teacher.tsx
@@ -1,7 +1,57 @@
-export default function Dashboard() {
+
+
+function Teacher() {
     return (
         <div className="p-6">
-            <h1>teacher</h1>
+            <div className="grid grid-cols-2 gap-4 mb-4">
+
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    value=""
+                    className="border p-2 rounded"
+                />
+                <input
+                    type="text"
+                    name="nic"
+                    placeholder="NIC"
+                    value=""
+                    className="border p-2 rounded"
+                />
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value=""
+                    className="border p-2 rounded"
+                />
+                <input
+                    type="text"
+                    name="phone"
+                    placeholder="Phone"
+                    value=""
+                    className="border p-2 rounded"
+                />
+                <input
+                    type="text"
+                    name="address"
+                    placeholder="Address"
+                    value=""
+                    className="border p-2 rounded"
+                />
+                <input
+                    type="text"
+                    name="subject"
+                    placeholder="Subject"
+                    value=""
+                    className="border p-2 rounded"
+                />
+            </div>
+
+
         </div>
     )
 }
+
+export default Teacher;
