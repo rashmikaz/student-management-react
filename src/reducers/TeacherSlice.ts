ea3bfa5bfee0d48eb677529d40cfa88249af0626
rashmikaz <phrashmika@gmail.com>
2025-02-23 10:01:28 +0530

    save teacher on reducers

--- src/reducers/TeacherSlice.ts
+++ src/reducers/TeacherSlice.ts
@@ -1,6 +1,8 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { TeacherModel } from "../models/TeacherModel.ts.ts";
 import axios from "axios";
+import {StudentModel} from "../models/StudentModel.ts";
+import {getStudents} from "./StudentSlice.ts";
 
 const initialState: TeacherModel[] = [];
 
@@ -16,6 +18,19 @@ export const getTeachers = createAsyncThunk(
             return response.data;
         } catch (error: any) {
             return rejectWithValue(error.response?.data || "Failed to load teacher");
+        }
+    }
+);
+
+export const saveTeacher = createAsyncThunk(
+    "teacher/saveTeacher",
+    async (teacher: TeacherModel, { dispatch, rejectWithValue }) => {
+        try {
+            const response = await api.post("/Teacher/add", teacher);
+            dispatch(getTeachers()); // Fetch updated teachers list
+            return response.data;
+        } catch (error: any) {
+            return rejectWithValue(error.response?.data || "Failed to save teacher");
         }
     }
 );
