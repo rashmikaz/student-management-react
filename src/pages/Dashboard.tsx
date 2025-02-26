ea7212e8a04ce12b8ad5841084c2de6b165f7d9a
rashmikaz <phrashmika@gmail.com>
2025-02-27 02:57:57 +0530

    image dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,18 @@
 export default function Dashboard() {
     return (
-        <div className="p-6">
-            <h1>Dashboard</h1>
+        <div>
+            <div>
+                <h1 className="font-mono text-6xl font-semibold absolute top-7 right-60 size-18">Welcome back</h1>
+                <p className="w-68 absolute top-37 left-290 size-16 break-all">"Effortless student management with real-time
+                    insights, attendance tracking, and performance analytics—all in one powerful dashboard!"</p>
+            </div>
+            <div>
+                <img src="src/assets/login.jpg" alt="" width="700" height="700"/>
+            </div>
+            <div className="absolute right-0 top-100">
+                <img src="src/assets/dashboard.jpg" alt="" width="400" height="400"/>
+            </div>
+
         </div>
     )
 }
